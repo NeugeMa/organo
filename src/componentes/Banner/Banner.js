ddb07ebf8d0821190255a219cd06d4b46b88e004
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-06 22:48:38 -0300

    iniciando projeto organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner() {         // function = função 
+    
+    // JSX 
+    return ( 
+        <header className="banner "> 
+        <img src="/imagens/banner.jpg" alt="O banner principal da página do Organo "/> 
+        </header>
+    )
+
+}
+
+export default Banner
