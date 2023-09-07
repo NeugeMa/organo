6cee9d064be6ca3a04c64d84c127362b7f43ac8d
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-07 20:40:04 -0300

    criaçao pasta/arquivo CampoTexto para se iniciar um formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() { // componente de react 
   return (
     <div className="App"> 
-      <Banner />   
+      <Banner />  
+      <CampoTexto label="Nome"/> 
+      <CampoTexto label="Cargo"/> 
+      <CampoTexto label="Imagem"/>  
     </div>
   );
 }
