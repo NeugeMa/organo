8da4e9779c186107d0c61c37cccc48786ef56e3c
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-08 18:06:24 -0300

    Primeiro, ajuste o CampoTexto para receber as props relacionadas. Depois, crie o componente ListaSuspensa para permitir que o usuário selecione o time do colaborador.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -10,7 +10,7 @@ const CampoTexto = (props) => {
                 {props.label}
             </label>
             
-            <input placeholder={placeholderModifica}/>
+            <input required={props.obrigatorio} placeholder={placeholderModifica}/>
         </div>
     )
 }
